6918b1c27084b5728a4e2438553e31469aab593b
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-03-03 07:25:23 +0900

    Add test target

---
+++ YMNetworkActivityManagerTests/YMNetworkActivityManagerTests.m
@@ -0,0 +1,31 @@
+//
+//  YMNetworkActivityManagerTests.m
+//  YMNetworkActivityManagerTests
+//
+//  Created by Yusuke Miyazaki on 2014/03/03.
+//  Copyright (c) 2014 Yusuke Miyazaki. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface YMNetworkActivityManagerTests : SenTestCase
+
+@end
+
+@implementation YMNetworkActivityManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
