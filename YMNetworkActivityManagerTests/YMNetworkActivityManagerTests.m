cfbce865867dc160111e1d9fb3dce9824ef94386
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-03-03 07:59:25 +0900

    Add test of sharedManager

--- YMNetworkActivityManagerTests/YMNetworkActivityManagerTests.m
+++ YMNetworkActivityManagerTests/YMNetworkActivityManagerTests.m
@@ -8,6 +8,8 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 
+#import "YMNetworkActivityManager.h"
+
 @interface YMNetworkActivityManagerTests : SenTestCase
 
 @end
@@ -16,16 +18,17 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testSharedManager {
+    YMNetworkActivityManager *manager;
+    manager = [YMNetworkActivityManager sharedManager];
+    STAssertNotNil(manager, @"Manager is nil.");
+    STAssertEqualObjects(manager, [YMNetworkActivityManager sharedManager], @"");
 }
 
 @end
