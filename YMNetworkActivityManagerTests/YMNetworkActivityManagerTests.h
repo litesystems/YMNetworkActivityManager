0e54f95a3944cf68ebb3329cf219e18ac8f57159
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-03-03 12:25:13 +0900

    Update project

---
+++ YMNetworkActivityManagerTests/YMNetworkActivityManagerTests.h
@@ -0,0 +1,12 @@
+//
+//  YMNetworkActivityManagerTests.h
+//  YMNetworkActivityManagerTests
+//
+//  Copyright (c) 2014 Yusuke Miyazaki. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface YMNetworkActivityManagerTests : SenTestCase
+
+@end
