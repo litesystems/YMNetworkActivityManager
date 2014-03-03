f54a4a24ebff1682eccc2d2e93e0d767636282f6
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-03-03 13:02:56 +0900

    Add demo project

---
+++ Demo/YMNetworkActivityManagerDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YMNetworkActivityManagerDemo
+//
+//  Copyright (c) 2013-2014 Yusuke Miyazaki.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
